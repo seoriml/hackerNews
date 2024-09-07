10c30e21cf658fc9cb0a2e49821d1dbbcf0bb048
Seorim Lee <esr619@naver.com>
2024-09-07 16:30:33 +0900

    데이터 호출 및 목록 상세, 라우터함수처리까지 복습

--- app.js
+++ app.js
@@ -1,78 +1,62 @@
-const container = document.getElementById('root');
-const ajax = new XMLHttpRequest();
-const content = document.createElement('div');
-const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
-const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
-const store = {
-  currentPage: 1,
-};
+const container = document.getElementById("root"); // 'root'가 반복되지않도록
+const ajax = new XMLHttpRequest(); // 데이터 변경하지 않을 것이므로 상수.(let은 변수)
+const content = document.createElement("div");
+const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
+const CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json";
 
 function getData(url) {
-  ajax.open('GET', url, false);
+  ajax.open("GET", url, false); // url, async boolean값(동기처리)
   ajax.send();
 
   return JSON.parse(ajax.response);
 }
 
+/* 목록화면 코드를 함수로 묶어서 재활용 */
 function newsFeed() {
-  const newsFeed = getData(NEWS_URL);
+  const newsFeed = getData(NEWS_URL); // getData함수가 필요한 url만 넘겨줌
   const newsList = [];
-  let template = `
-    <div class="container mx-auto p-4">
-      <h1>Hacker News</h1>
-      <ul>
-        {{__news_feed__}}      
-      </ul>
-      <div>
-        <a href="#/page/{{__prev_page__}}">이전 페이지</a>
-        <a href="#/page/{{__next_page__}}">다음 페이지</a>
-      </div>
-    </div>
-  `;
+  newsList.push("<ul>");
 
-  for(let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
+  for (let i = 0; i < 10; i++) {
     newsList.push(`
       <li>
-        <a href="#/show/${newsFeed[i].id}">
+        <a href="#${newsFeed[i].id}">
           ${newsFeed[i].title} (${newsFeed[i].comments_count})
         </a>
       </li>
     `);
   }
 
-  template = template.replace('{{__news_feed__}}', newsList.join(''));
-  template = template.replace('{{__prev_page__}}', store.currentPage > 1 ? store.currentPage - 1 : 1);
-  template = template.replace('{{__next_page__}}', store.currentPage + 1);
-  
-  container.innerHTML = template;
+  newsList.push("</ul>");
+
+  container.innerHTML = newsList.join(""); //태그들을 하나의 문자열로 합쳐서 반환
 }
 
+/* 해시가 변경될때 실행되는 상세화면 함수 */
 function newsDetail() {
-  const id = location.hash.substr(7);
-  const newsContent = getData(CONTENT_URL.replace('@id', id))
+  const id = location.hash.substr(1); //#이 제거된 숫자값만 할당
+  const newsContent = getData(CONTENT_URL.replace("@id", id)); //getData함수가 필요한 url만 넘겨줌
 
   container.innerHTML = `
     <h1>${newsContent.title}</h1>
 
     <div>
-      <a href="#/page/${store.currentPage}">목록으로</a>
+      <a href="#">목록으로</a>
     </div>
   `;
 }
 
+/* 화면을 호출하는 라우터처리 함수 */
 function router() {
-  const routePath = location.hash;
+  const routePath = location.hash; // 해시값을 가져와서 목록인지 상세인지 호출
 
-  if (routePath === '') {
-    newsFeed();
-  } else if (routePath.indexOf('#/page/') >= 0) {
-    store.currentPage = Number(routePath.substr(7));
+  if (routePath === "") {
+    //location.hash에 #만 있으면 빈값을 반환하므로 참.
     newsFeed();
   } else {
-    newsDetail()
+    newsDetail();
   }
 }
 
-window.addEventListener('hashchange', router);
-
+window.addEventListener("hashchange", router); //화면이 전환될때 router함수 호출
 router();
