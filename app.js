bbd4cab5d86b705380bfa981268e8af43053a34e
Seorim Lee <esr619@naver.com>
2024-09-08 19:06:11 +0900

    페이징 구현 복습

--- app.js
+++ app.js
@@ -1,62 +1,81 @@
-const container = document.getElementById("root"); // 'root'가 반복되지않도록
-const ajax = new XMLHttpRequest(); // 데이터 변경하지 않을 것이므로 상수.(let은 변수)
-const content = document.createElement("div");
-const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
-const CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json";
+const container = document.getElementById("root"); // 'root' 요소를 가져와서 변수에 할당 (중복 방지)
+const ajax = new XMLHttpRequest(); // 데이터 변경이 없으므로 const 사용 (let은 변수를 위한 키워드)
+const content = document.createElement("div"); // 새로운 div 요소 생성
+const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json"; // 뉴스 데이터를 가져올 API URL
+const CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json"; // 뉴스 상세 데이터를 가져올 API URL (@id는 실제 ID로 대체됨)
 
+const store = {
+  currentPage: 1, // 현재 페이지를 저장하는 객체 (기본값: 1)
+};
+
+/* 데이터를 가져오는 함수 */
 function getData(url) {
-  ajax.open("GET", url, false); // url, async boolean값(동기처리)
-  ajax.send();
+  ajax.open("GET", url, false); // 비동기 호출을 사용하지 않고, 동기적으로 데이터를 가져옴
+  ajax.send(); // 요청을 서버에 전송
 
-  return JSON.parse(ajax.response);
+  return JSON.parse(ajax.response); // 서버 응답을 JSON 형식으로 변환하여 반환
 }
 
-/* 목록화면 코드를 함수로 묶어서 재활용 */
+/* 뉴스 목록을 화면에 표시하는 함수 */
 function newsFeed() {
-  const newsFeed = getData(NEWS_URL); // getData함수가 필요한 url만 넘겨줌
+  const newsFeed = getData(NEWS_URL); // 뉴스 목록 데이터를 가져오기위해 getData함수가 필요한 url만 넘겨줌
   const newsList = [];
-  newsList.push("<ul>");
+  newsList.push("<ul>"); // 목록 시작 태그 추가
 
-  for (let i = 0; i < 10; i++) {
+  // 현재 페이지에 맞는 뉴스 10개를 리스트에 추가
+  for (let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
     newsList.push(`
       <li>
-        <a href="#${newsFeed[i].id}">
+        <a href="#/show/${newsFeed[i].id}">
           ${newsFeed[i].title} (${newsFeed[i].comments_count})
         </a>
       </li>
     `);
   }
 
-  newsList.push("</ul>");
-
-  container.innerHTML = newsList.join(""); //태그들을 하나의 문자열로 합쳐서 반환
+  newsList.push("</ul>"); // 목록 끝 태그 추가
+  newsList.push(`
+    <div>
+      <a href="#/page/${
+        store.currentPage > 1 ? store.currentPage - 1 : 1
+      }">이전 페이지</a>
+      <a href="#/page/${store.currentPage + 1}">다음 페이지</a>
+    </div>
+  `);
+  // 이전 페이지 링크와 다음 페이지 링크를 추가 (이전 페이지는 1 이하로 내려가지 않도록 방어 코드 추가)
+  container.innerHTML = newsList.join(""); // 뉴스 목록을 화면에 표시 (배열을 문자열로 결합)
 }
 
-/* 해시가 변경될때 실행되는 상세화면 함수 */
+/* 해시가 변경될때 뉴스 상세 내용을 화면에 표시하는 함수 */
 function newsDetail() {
-  const id = location.hash.substr(1); //#이 제거된 숫자값만 할당
-  const newsContent = getData(CONTENT_URL.replace("@id", id)); //getData함수가 필요한 url만 넘겨줌
+  const id = location.hash.substr(7); // 해시에서 '#/show/' 이후의 ID 부분을 추출
+  const newsContent = getData(CONTENT_URL.replace("@id", id)); //getData함수가 필요한 url만 넘겨줌, ID를 사용하여 상세 데이터를 가져옴
 
   container.innerHTML = `
     <h1>${newsContent.title}</h1>
 
     <div>
-      <a href="#">목록으로</a>
+      <a href="#/page/${store.currentPage}">목록으로</a>
     </div>
   `;
+  // 현재페이지를 알기위해 #/page/${store.currentPage}로 이동
 }
 
-/* 화면을 호출하는 라우터처리 함수 */
+/* 라우터 함수: 해시값에 따라 화면을 전환 */
 function router() {
-  const routePath = location.hash; // 해시값을 가져와서 목록인지 상세인지 호출
+  const routePath = location.hash; // 현재 URL의 해시값을 가져옴
 
   if (routePath === "") {
-    //location.hash에 #만 있으면 빈값을 반환하므로 참.
+    // 해시값이 없으면 뉴스 목록 화면 표시 (location.hash에 #만 있으면 빈값을 반환하므로 참)
     newsFeed();
+  } else if (routePath.indexOf("#/page/") >= 0) {
+    // 해시값에 '#/page/'가 포함되어 있으면 페이지 전환 처리 (indexof메소드는 문자열을 찾아서 없으면 0보다 작은값 반환)
+    store.currentPage = Number(routePath.substr(7)); // 페이지 번호 추출 후 숫자로 변환하여 저장
+    newsFeed(); // 해당 페이지의 뉴스 목록을 표시
   } else {
-    newsDetail();
+    newsDetail(); // 그 외의 경우 뉴스 상세 화면 표시
   }
 }
 
-window.addEventListener("hashchange", router); //화면이 전환될때 router함수 호출
-router();
+window.addEventListener("hashchange", router); // 해시값 변경 시 router 함수 호출
+router(); // 초기 로딩 시 router 함수 호출
